2a1896f3bde6afcb362164b615a224338f0b71c2
Hochul Shin <dakoo@users.noreply.github.com>
2015-07-16 13:46:21 +0900

    Update user.js

--- server/plugins/user.js
+++ server/plugins/user.js
@@ -2,13 +2,10 @@ var Boom = require('boom');
 exports.add = function (request, reply) {
     var db = request.server.plugins['hapi-mongodb'].db;
     db.collection('users').findOne({"email" : request.payload.email}, function(err, ret) {
-        console.log("error: " + err + " return: " + ret);
         if (err) {
-            console.log("Error:query");
             return reply(Boom.internal('Internal Database Error', err));
         }
         if (ret){
-            console.log("Error: duplicated resource");
             return reply(Boom.conflict('Duplicated Resource Error', err));
         }
         var user = {
@@ -18,14 +15,12 @@ exports.add = function (request, reply) {
         };
         db.collection('users').insert(user, {w:1}, function (err){
             if (err){
-                console.log("Error:insert");
                 return reply(Boom.internal('Internal Database Error', err));
             }else{
-                console.log("success");
                 reply();
             }
         });
-    })
+    });
 };
 exports.getlist = function (request, reply) {
     var db = request.server.plugins['hapi-mongodb'].db;
